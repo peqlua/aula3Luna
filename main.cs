fd2654737fa453b30f5edcd22691d276bbbf00da
Luna <128090844+peqlua@users.noreply.github.com>
2024-08-19 13:48:45 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+
+  string[] brinquedos = {"carbon","lego","unicornio","peluxe","telemove",};
+    
+    Console.WriteLine (brinquedos[4]);
+  }
+}
